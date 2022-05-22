016e0a9e6c9b01ea0c5c4fd19620a230b7cb1dc9
Claudia Sanchez <csanchezm16@gmail.com>
2022-05-22 21:31:02 +0200

    avegareds

--- matlab/TestSimulatoin.m
+++ matlab/TestSimulatoin.m
@@ -2,70 +2,68 @@
 M = readtable( "data.csv" );
 R = readtable("dataReg.csv");
 T = readtable("dataT.csv");
+Q = readtable("queueData.csv");
 
 meanRegArrivals = mean(R.Arrivals);
+
 %%checking if arrivals are poisson distributed 
 sortedRArrivales = sort(R.Arrivals);
 g =  zeros(1, length(sortedRArrivales));
 for i = 1  : length(sortedRArrivales)-1
     g(i) = sortedRArrivales(i+1) - sortedRArrivales(i);
 end 
-figure 
-% histogram(g)
-% hold on
-x = (0:1:650);
-p = poisspdf(x, 60);
-% plot(x,p, 'g-','LineWidth',2)
-histogram(p)
-title("regular arrivals")
+% figure 
+% % histogram(g)
+% % hold on
+% x = (0:1:650);
+% p = poisspdf(x, 60);
+% % plot(x,p, 'g-','LineWidth',2)
+% histogram(p)
+% title("regular arrivals")
 
 
 
 %%
-delayReg =  R.ArrivalsAtMachine - R.Arrivals;
+delayR =  R.ArrivalsAtMachine - R.Arrivals;
 delayT=  T.ArrivalsAtMachine - T.Arrivals;
 
 %%z score for 95 confidence interval = 1.96
-CI95 = [mean(delayT) - 1.96*(std(delayT)./sqrt(numel(delayT))), mean(delayT) + 1.96*(std(delayT)./sqrt(numel(delayT)))];
+
+% https://www.google.com/search?q=confidence+interval+formula&oq=confidence+interval+&aqs=chrome.2.69i57j0i67l4j69i60l3.5344j0j7&sourceid=chrome&ie=UTF-8
+CI95R = [mean(delayR) - 1.96*(std(delayR)./sqrt(numel(delayR))), mean(delayR) + 1.96*(std(delayR)./sqrt(numel(delayR)))];
+CI95T = [mean(delayT) - 1.96*(std(delayT)./sqrt(numel(delayT))), mean(delayT) + 1.96*(std(delayT)./sqrt(numel(delayT)))];
 
 figure 
-histogram(delayT, 15)
+histogram(delayR, 15)
+hold on 
+ylim = get(gca, 'YLim');
+plot(mean(delayR)*[1,1],ylim*1.05,'r-','LineWidth',2);
+plot(CI95R(1)*[1,1],ylim*1.05,'g-','LineWidth',2);
+plot(CI95R(2)*[1,1],ylim*1.05,'g-','LineWidth',2);
+title("delay REGULAR cust")
+
+
+figure 
+histogram(delayT, 20)
 hold on 
 ylim = get(gca, 'YLim');
 plot(mean(delayT)*[1,1],ylim*1.05,'r-','LineWidth',2);
-plot(CI95(1)*[1,1],ylim*1.05,'g-','LineWidth',2);
-plot(CI95(2)*[1,1],ylim*1.05,'g-','LineWidth',2);
+plot(CI95T(1)*[1,1],ylim*1.05,'g-','LineWidth',2);
+plot(CI95T(2)*[1,1],ylim*1.05,'g-','LineWidth',2);
 title("delay service cust")
 
-figure 
 
-histogram(delayReg)
-title("delay regular cust ")
-
-
-
-y = exppdf(x,meanREg);
-
-
-
-
-
-
-% https://www.google.com/search?q=confidence+interval+formula&oq=confidence+interval+&aqs=chrome.2.69i57j0i67l4j69i60l3.5344j0j7&sourceid=chrome&ie=UTF-8
-% 95% confidence level value : 1.96
-sortedT = sort(T.Arrivals);
-figure 
-histogram(sortedT)
-title("arrival for ta")
+% figure 
+% scatterplot(delayT)
+% title('dela')
+% 
+%one column is for customers
+% lengths = zeros(size(Q, 2)) -1;
+% for i = 2 : length(lengths)
+%     h = Q(:,i);
+%     sum = sum(h);
+%     lengths(i) = sum;
+% end 
 
 
-% figure
-% histogram(delayT)
-% hold on 
-% ylim = get(gca, 'YLim');
-% plot(mean(delayT)*[1,1],ylim*1.05,'r-','LineWidth',2);
-% plot(CI95(1)*[1,1],ylim*1.05,'g-','LineWidth',2);
-% plot(CI95(2)*[1,1],ylim*1.05,'g-','LineWidth',2);
-% hold off
-% title("delay time with confidence ")
 
