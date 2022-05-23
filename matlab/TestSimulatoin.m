f5c123ddc38701b8920d8bee8719e37d75f4dde9
Claudia Sanchez <csanchezm16@gmail.com>
2022-05-23 08:35:40 +0200

    comments in matlab

--- matlab/TestSimulatoin.m
+++ matlab/TestSimulatoin.m
@@ -1,3 +1,8 @@
+%This data represents the averages of already 101 simulations runned of the
+%same system( please do not misconfuse the header of Customers of the csv
+%file since this is just because we did not change the headers string when
+%we computed this data 
+
 regular = readtable("dataReg.csv");
 serviceDesk = readtable("dataT.csv");
 overall = readtable("dataO.csv");
@@ -11,7 +16,14 @@ machineTimeReg = regular.ArrivalsAtMachine;
 machineTimeT = serviceDesk.ArrivalsAtMachine;
 machineTimeO = overall.ArrivalsAtMachine;
 
+%%given that we performed 101 simulations and then we recorded the each of
+%%the averages of teh arrivals, at machine and departure we can say that
+%%we have 100 degrees of freedom, and we have 0.95 as significance level,
+%%so looking it up in the t-test student score test (its the one he uses in
+%%the lecture videos) we obtained the coeff 1.066
+
 n = 101;
+
 
 delayR =  machineTimeReg - arrivalTimeReg;
 [mR, CIR] = CIplot(delayR,n, 1.066, "delay regular customer");
